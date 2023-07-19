82b8e22ace3c5596dc5a30191f4b61910a77f365
pandaTTTT-1 <seventeen171098@gmail.com>
2023-07-19 13:51:14 +0700

    frontend

--- MainBackend/README.md
+++
@@ -1,2 +0,0 @@
-# Sem2FPT
-T2205M-Sem2-finalProject-NgocDung-NgocHung-VietAnh-MinhHoang
--- MainBackend/app/routers/user.router.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-    var userController = require("../controllers/user.controller"); // controller
-    var middleware = require("../commons/Author_middleware");
-
-    app.get("/users/list", userController.list_user); //middleware.verifyTokenIsAdmin, 
-    app.get("/users/detail/:id",  userController.detail_user);
-    app.delete("/users/delete/:id", userController.delete_user);
-    app.put("/users/update",  userController.change_password);
-}
